024146398c2008c843b2ced86bb60b5463cfea58
wenghongtian <wenghongtian@gmail.com>
2023-05-21 23:36:41 +0800

    feat: 3d矩阵变换&zBuffer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,119 @@
 import "./style.css";
-// import * as webglUtils from "./webglUtils";
+
+const m4 = {
+  translation(tx: number, ty: number, tz: number) {
+    return [1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, tx, ty, tz, 1];
+  },
+  xRotation(angle: number) {
+    const c = Math.cos(angle);
+    const s = Math.sin(angle);
+    return [1, 0, 0, 0, 0, c, s, 0, 0, -s, c, 0, 0, 0, 0, 1];
+  },
+  yRotation(angle: number) {
+    const c = Math.cos(angle);
+    const s = Math.sin(angle);
+    return [c, 0, -s, 0, 0, 1, 0, 0, s, 0, c, 0, 0, 0, 0, 1];
+  },
+  zRotation(angle: number) {
+    const c = Math.cos(angle);
+    const s = Math.sin(angle);
+    return [c, s, 0, 0, -s, c, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1];
+  },
+  scaling(sx: number, sy: number, sz: number) {
+    return [sx, 0, 0, 0, 0, sy, 0, 0, 0, 0, sz, 0, 0, 0, 0, 1];
+  },
+  multiply: function (a: number[], b: number[]) {
+    var a00 = a[0 * 4 + 0];
+    var a01 = a[0 * 4 + 1];
+    var a02 = a[0 * 4 + 2];
+    var a03 = a[0 * 4 + 3];
+    var a10 = a[1 * 4 + 0];
+    var a11 = a[1 * 4 + 1];
+    var a12 = a[1 * 4 + 2];
+    var a13 = a[1 * 4 + 3];
+    var a20 = a[2 * 4 + 0];
+    var a21 = a[2 * 4 + 1];
+    var a22 = a[2 * 4 + 2];
+    var a23 = a[2 * 4 + 3];
+    var a30 = a[3 * 4 + 0];
+    var a31 = a[3 * 4 + 1];
+    var a32 = a[3 * 4 + 2];
+    var a33 = a[3 * 4 + 3];
+    var b00 = b[0 * 4 + 0];
+    var b01 = b[0 * 4 + 1];
+    var b02 = b[0 * 4 + 2];
+    var b03 = b[0 * 4 + 3];
+    var b10 = b[1 * 4 + 0];
+    var b11 = b[1 * 4 + 1];
+    var b12 = b[1 * 4 + 2];
+    var b13 = b[1 * 4 + 3];
+    var b20 = b[2 * 4 + 0];
+    var b21 = b[2 * 4 + 1];
+    var b22 = b[2 * 4 + 2];
+    var b23 = b[2 * 4 + 3];
+    var b30 = b[3 * 4 + 0];
+    var b31 = b[3 * 4 + 1];
+    var b32 = b[3 * 4 + 2];
+    var b33 = b[3 * 4 + 3];
+    return [
+      b00 * a00 + b01 * a10 + b02 * a20 + b03 * a30,
+      b00 * a01 + b01 * a11 + b02 * a21 + b03 * a31,
+      b00 * a02 + b01 * a12 + b02 * a22 + b03 * a32,
+      b00 * a03 + b01 * a13 + b02 * a23 + b03 * a33,
+      b10 * a00 + b11 * a10 + b12 * a20 + b13 * a30,
+      b10 * a01 + b11 * a11 + b12 * a21 + b13 * a31,
+      b10 * a02 + b11 * a12 + b12 * a22 + b13 * a32,
+      b10 * a03 + b11 * a13 + b12 * a23 + b13 * a33,
+      b20 * a00 + b21 * a10 + b22 * a20 + b23 * a30,
+      b20 * a01 + b21 * a11 + b22 * a21 + b23 * a31,
+      b20 * a02 + b21 * a12 + b22 * a22 + b23 * a32,
+      b20 * a03 + b21 * a13 + b22 * a23 + b23 * a33,
+      b30 * a00 + b31 * a10 + b32 * a20 + b33 * a30,
+      b30 * a01 + b31 * a11 + b32 * a21 + b33 * a31,
+      b30 * a02 + b31 * a12 + b32 * a22 + b33 * a32,
+      b30 * a03 + b31 * a13 + b32 * a23 + b33 * a33,
+    ];
+  },
+  identity() {
+    return [1, 0, 0, 0, 1, 0, 0, 0, 1];
+  },
+  projection(width: number, height: number, depth: number) {
+    return [
+      2 / width,
+      0,
+      0,
+      0,
+      0,
+      -2 / height,
+      0,
+      0,
+      0,
+      0,
+      2 / depth,
+      0,
+      -1,
+      1,
+      0,
+      1,
+    ];
+  },
+
+  translate(m: number[], tx: number, ty: number, tz: number) {
+    return m4.multiply(m, m4.translation(tx, ty, tz));
+  },
+  xRotate(m: number[], angle: number) {
+    return m4.multiply(m, m4.xRotation(angle));
+  },
+  yRotate(m: number[], angle: number) {
+    return m4.multiply(m, m4.yRotation(angle));
+  },
+  zRotate(m: number[], angle: number) {
+    return m4.multiply(m, m4.zRotation(angle));
+  },
+  scale(m: number[], sx: number, sy: number, sz: number) {
+    return m4.multiply(m, m4.scaling(sx, sy, sz));
+  },
+};
 
 function main() {
   const canvas = document.querySelector(`#canvas`) as HTMLCanvasElement;
@@ -11,26 +125,21 @@ function main() {
   ]);
   gl.useProgram(program);
 
-  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const positionLocation = gl.getAttribLocation(program, "a_position");
+  const colorLocation = gl.getAttribLocation(program, "a_color");
+  const matrixLocation = gl.getUniformLocation(program, "u_matrix");
 
-  const resolutionUniformLocation = gl.getUniformLocation(
-    program,
-    "u_resolution"
-  );
-  const colorUniformLocation = gl.getUniformLocation(program, "u_color");
-  const translationLocation = gl.getUniformLocation(program, "u_translation");
-  const rotationLocation = gl.getUniformLocation(program, "u_rotation");
-  const scaleLocation = gl.getUniformLocation(program, "u_scale");
+  const translation = [0, 0, 0];
+  const rotation = [0, 0, 0];
+  const scale = [1, 1, 1];
 
   const positionBuffer = gl.createBuffer();
-
-  const translation = [0, 0];
-  const rotation = [0, 1];
-  const scale = [1, 1];
-  const color = [Math.random(), Math.random(), Math.random(), 1];
-
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
   setGeometry(gl);
+
+  const colorBuffer = gl.createBuffer();
+  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+  setColors(gl);
 
   drawScene();
 
@@ -42,8 +151,20 @@ function main() {
     slide: updatePosition(1),
     max: gl.canvas.height,
   });
-  window.webglLessonsUI.setupSlider("#angle", {
-    slide: updateAngle,
+  window.webglLessonsUI.setupSlider("#z", {
+    slide: updatePosition(2),
+    max: gl.canvas.height,
+  });
+  window.webglLessonsUI.setupSlider("#angleX", {
+    slide: updateAngle(0),
+    max: 360,
+  });
+  window.webglLessonsUI.setupSlider("#angleY", {
+    slide: updateAngle(1),
+    max: 360,
+  });
+  window.webglLessonsUI.setupSlider("#angleZ", {
+    slide: updateAngle(2),
     max: 360,
   });
   window.webglLessonsUI.setupSlider("#scaleX", {
@@ -62,15 +183,13 @@ function main() {
     step: 0.01,
     precision: 2,
   });
-  window.$("#rotation").gmanUnitCircle({
-    width: 200,
-    height: 200,
-    value: 0,
-    slide(e: any, u: { x: number; y: number }) {
-      rotation[0] = u.x;
-      rotation[1] = u.y;
-      drawScene();
-    },
+  window.webglLessonsUI.setupSlider("#scaleZ", {
+    value: scale[2],
+    slide: updateScale(2),
+    min: -5,
+    max: 5,
+    step: 0.01,
+    precision: 2,
   });
 
   function updateScale(index: number) {
@@ -87,115 +206,193 @@ function main() {
     };
   }
 
-  function updateAngle(event: any, ui: { value: number }) {
-    var angleInDegrees = 360 - ui.value;
-    const angleRadians = (angleInDegrees / 180) * Math.PI;
-    rotation[0] = Math.sin(angleRadians);
-    rotation[1] = Math.cos(angleRadians);
-    drawScene();
+  function updateAngle(index: number) {
+    return (event: any, ui: { value: number }) => {
+      rotation[index] = degToRad(ui.value);
+      drawScene();
+    };
   }
 
   function drawScene() {
+    gl.enable(gl.CULL_FACE);
+    gl.enable(gl.DEPTH_TEST);
     window.webglUtils.resizeCanvasToDisplaySize(gl.canvas as HTMLCanvasElement);
 
     // 告诉WebGL如何从裁剪空间对应到像素
-    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+    gl.viewport(
+      0,
+      0,
+      (gl.canvas as HTMLCanvasElement).clientWidth,
+      (gl.canvas as HTMLCanvasElement).clientHeight
+    );
 
     // 清空画布
-    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
     // 使用我们的程序
     gl.useProgram(program);
 
     // 启用属性
-    gl.enableVertexAttribArray(positionAttributeLocation);
-
-    // 绑定位置缓冲
+    gl.enableVertexAttribArray(positionLocation);
     gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+    gl.vertexAttribPointer(positionLocation, 3, gl.FLOAT, false, 0, 0);
 
-    // setRectangle(gl, translation[0], translation[1], width, height);
+    gl.enableVertexAttribArray(colorLocation);
+    gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
+    gl.vertexAttribPointer(colorLocation, 3, gl.UNSIGNED_BYTE, true, 0, 0);
 
-    gl.uniform2fv(translationLocation, translation);
+    let matrix = m4.projection(
+      (gl.canvas as HTMLCanvasElement).clientWidth,
+      (gl.canvas as HTMLCanvasElement).clientHeight,
+      400
+    );
+    matrix = m4.translate(
+      matrix,
+      translation[0],
+      translation[1],
+      translation[2]
+    );
+    matrix = m4.xRotate(matrix, rotation[0]);
+    matrix = m4.yRotate(matrix, rotation[1]);
+    matrix = m4.zRotate(matrix, rotation[2]);
+    matrix = m4.scale(matrix, scale[0], scale[1], scale[2]);
 
-    // 告诉属性怎么从positionBuffer中读取数据 (ARRAY_BUFFER)
-    gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
-
-    // 设置缩放
-    gl.uniform2fv(scaleLocation, scale);
-
-    // 设置分辨率
-    gl.uniform2f(resolutionUniformLocation, gl.canvas.width, gl.canvas.height);
-
-    // 设置颜色
-    gl.uniform4fv(colorUniformLocation, color);
-
-    // 设置位移
-    gl.uniform2fv(translationLocation, translation);
-
-    // 设置旋转
-    gl.uniform2fv(rotationLocation, rotation);
-
+    gl.uniformMatrix4fv(matrixLocation, false, matrix);
     // 绘制矩形
-    gl.drawArrays(gl.TRIANGLES, 0, 18);
+    gl.drawArrays(gl.TRIANGLES, 0, 96);
   }
 }
 
-function randomInt(range: number) {
-  return Math.floor(Math.random() * range);
+function setColors(gl: WebGLRenderingContext) {
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Uint8Array([
+      // left column front
+      200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200,
+      70, 120,
+
+      // top rung front
+      200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200,
+      70, 120,
+
+      // middle rung front
+      200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200, 70, 120, 200,
+      70, 120,
+
+      // left column back
+      80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70,
+      200,
+
+      // top rung back
+      80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70,
+      200,
+
+      // middle rung back
+      80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70, 200, 80, 70,
+      200,
+
+      // top
+      70, 200, 210, 70, 200, 210, 70, 200, 210, 70, 200, 210, 70, 200, 210, 70,
+      200, 210,
+
+      // top rung right
+      200, 200, 70, 200, 200, 70, 200, 200, 70, 200, 200, 70, 200, 200, 70, 200,
+      200, 70,
+
+      // under top rung
+      210, 100, 70, 210, 100, 70, 210, 100, 70, 210, 100, 70, 210, 100, 70, 210,
+      100, 70,
+
+      // between top rung and middle
+      210, 160, 70, 210, 160, 70, 210, 160, 70, 210, 160, 70, 210, 160, 70, 210,
+      160, 70,
+
+      // top of middle rung
+      70, 180, 210, 70, 180, 210, 70, 180, 210, 70, 180, 210, 70, 180, 210, 70,
+      180, 210,
+
+      // right of middle rung
+      100, 70, 210, 100, 70, 210, 100, 70, 210, 100, 70, 210, 100, 70, 210, 100,
+      70, 210,
+
+      // bottom of middle rung.
+      76, 210, 100, 76, 210, 100, 76, 210, 100, 76, 210, 100, 76, 210, 100, 76,
+      210, 100,
+
+      // right of bottom
+      140, 210, 80, 140, 210, 80, 140, 210, 80, 140, 210, 80, 140, 210, 80, 140,
+      210, 80,
+
+      // bottom
+      90, 130, 110, 90, 130, 110, 90, 130, 110, 90, 130, 110, 90, 130, 110, 90,
+      130, 110,
+
+      // left side
+      160, 160, 220, 160, 160, 220, 160, 160, 220, 160, 160, 220, 160, 160, 220,
+      160, 160, 220,
+    ]),
+    gl.STATIC_DRAW
+  );
 }
 
 function setGeometry(gl: WebGLRenderingContext) {
   gl.bufferData(
     gl.ARRAY_BUFFER,
     new Float32Array([
-      0, 0, 30, 0, 0, 150, 0, 150, 30, 0, 30, 150,
+      // left column front
+      0, 0, 0, 0, 150, 0, 30, 0, 0, 0, 150, 0, 30, 150, 0, 30, 0, 0,
 
-      30, 0, 100, 0, 30, 30, 30, 30, 100, 0, 100, 30,
+      // top rung front
+      30, 0, 0, 30, 30, 0, 100, 0, 0, 30, 30, 0, 100, 30, 0, 100, 0, 0,
 
-      30, 60, 67, 60, 30, 90, 30, 90, 67, 60, 67, 90,
+      // middle rung front
+      30, 60, 0, 30, 90, 0, 67, 60, 0, 30, 90, 0, 67, 90, 0, 67, 60, 0,
+
+      // left column back
+      0, 0, 30, 30, 0, 30, 0, 150, 30, 0, 150, 30, 30, 0, 30, 30, 150, 30,
+
+      // top rung back
+      30, 0, 30, 100, 0, 30, 30, 30, 30, 30, 30, 30, 100, 0, 30, 100, 30, 30,
+
+      // middle rung back
+      30, 60, 30, 67, 60, 30, 30, 90, 30, 30, 90, 30, 67, 60, 30, 67, 90, 30,
+
+      // top
+      0, 0, 0, 100, 0, 0, 100, 0, 30, 0, 0, 0, 100, 0, 30, 0, 0, 30,
+
+      // top rung right
+      100, 0, 0, 100, 30, 0, 100, 30, 30, 100, 0, 0, 100, 30, 30, 100, 0, 30,
+
+      // under top rung
+      30, 30, 0, 30, 30, 30, 100, 30, 30, 30, 30, 0, 100, 30, 30, 100, 30, 0,
+
+      // between top rung and middle
+      30, 30, 0, 30, 60, 30, 30, 30, 30, 30, 30, 0, 30, 60, 0, 30, 60, 30,
+
+      // top of middle rung
+      30, 60, 0, 67, 60, 30, 30, 60, 30, 30, 60, 0, 67, 60, 0, 67, 60, 30,
+
+      // right of middle rung
+      67, 60, 0, 67, 90, 30, 67, 60, 30, 67, 60, 0, 67, 90, 0, 67, 90, 30,
+
+      // bottom of middle rung.
+      30, 90, 0, 30, 90, 30, 67, 90, 30, 30, 90, 0, 67, 90, 30, 67, 90, 0,
+
+      // right of bottom
+      30, 90, 0, 30, 150, 30, 30, 90, 30, 30, 90, 0, 30, 150, 0, 30, 150, 30,
+
+      // bottom
+      0, 150, 0, 0, 150, 30, 30, 150, 30, 0, 150, 0, 30, 150, 30, 30, 150, 0,
+
+      // left side
+      0, 0, 0, 0, 0, 30, 0, 150, 30, 0, 0, 0, 0, 150, 30, 0, 150, 0,
     ]),
     gl.STATIC_DRAW
   );
 }
 
-function setRectangle(
-  gl: WebGLRenderingContext,
-  x: number,
-  y: number,
-  width: number,
-  height: number
-) {
-  const x1 = x;
-  const x2 = x + width;
-  const y1 = y;
-  const y2 = y + height;
-
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
-    gl.STATIC_DRAW
-  );
-}
-
-function createShader(gl: WebGLRenderingContext, type: number, source: string) {
-  const shader = gl.createShader(type)!;
-  gl.shaderSource(shader, source);
-  gl.compileShader(shader);
-  gl.getShaderParameter(shader, gl.COMPILE_STATUS);
-  return shader;
-}
-
-function createProgram(
-  gl: WebGLRenderingContext,
-  vertexShader: WebGLShader,
-  fragmentShader: WebGLShader
-) {
-  const program = gl.createProgram()!;
-  gl.attachShader(program, vertexShader);
-  gl.attachShader(program, fragmentShader);
-  gl.linkProgram(program);
-  gl.getProgramParameter(program, gl.LINK_STATUS);
-  return program;
+function degToRad(d: number) {
+  return (d * Math.PI) / 180;
 }
 
 main();
